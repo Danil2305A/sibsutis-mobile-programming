9a951803f8d614f45c06d7e0215578254be48716
hikaruvi <daniilrybkin32@gmail.com>
2026-09-19 11:34:39 +0700

    feat: add authors screen implemenation

---
+++ app/src/main/java/com/example/bugs/tab/AuthorsScreen.kt
@@ -0,0 +1,63 @@
+package com.example.bugs.tab
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.bugs.R
+
+
+data class Author(val name: String, @DrawableRes val photo: Int)
+
+@Composable
+fun AuthorsScreen() {
+    val authors = remember {
+        listOf(
+            Author("Акуско Д.Д.", R.drawable.student_ak_danil),
+            Author("Рыбкин Д.А.", R.drawable.student_rb_daniil)
+        )
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(authors) { a ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(a.photo),
+                    contentDescription = a.name,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Spacer(Modifier.width(16.dp))
+                Text(a.name, style = MaterialTheme.typography.titleMedium)
+            }
+        }
+    }
+}
