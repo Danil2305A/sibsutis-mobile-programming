41a7b151bec6b6e7885c8331b86d4aa7aa034420
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-09-26 13:38:14 +0700

    feat: add tab icons

--- app/src/main/java/com/example/bugs/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/bugs/ui/screen/MainScreen.kt
@@ -1,24 +1,33 @@
 package com.example.bugs.ui.screen
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import com.example.bugs.R
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.unit.dp
+import com.example.bugs.R
 import com.example.bugs.data.repository.rememberGameSettings
 import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen() {
-    val tabs = stringArrayResource(R.array.tabs)
-    val pagerState = rememberPagerState(pageCount = { tabs.size })
+    val tabIcons = listOf(
+        R.drawable.auth,
+        R.drawable.settings,
+        R.drawable.rules,
+        R.drawable.authors,
+        R.drawable.game
+    )
+    val pagerState = rememberPagerState(pageCount = { tabIcons.size })
     val scope = rememberCoroutineScope()
 
     val gameSettings = rememberGameSettings()
@@ -26,11 +35,17 @@ fun MainScreen() {
     Scaffold(
         topBar = {
             TabRow(selectedTabIndex = pagerState.currentPage) {
-                tabs.forEachIndexed { i, title ->
+                tabIcons.forEachIndexed { i, iconRes ->
                     Tab(
                         selected = pagerState.currentPage == i,
                         onClick = { scope.launch { pagerState.animateScrollToPage(i) } },
-                        text = { Text(title) }
+                        icon = {
+                            Icon(
+                                painter = painterResource(iconRes),
+                                contentDescription = null,
+                                modifier = Modifier.size(30.dp)
+                            )
+                        }
                     )
                 }
             }
